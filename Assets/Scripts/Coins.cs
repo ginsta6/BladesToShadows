7cd262728e62c2e09a016aa07c05f01ec4bca2d2
ginsta6 <gintazzz9@gmail.com>
2020-06-24 20:50:16 +0300

    added buttons to sell items

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+    public static Coins instance;
+
+    #region Singleton
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+
+    public static int balance = 0;
+    public Text amount;
+    public Image icon;
+
+    public void UpdateUI()
+    {
+        amount.text = balance.ToString();
+    }
+
+    public void ItemSold(int price)
+    {
+        balance += price;
+    }
+
+
+
+}
---
+++ Assets/Scripts/InvenotorySlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvenotorySlot : MonoBehaviour
+{
+    public Image image;
+
+
+    public void SellItem()
+    {
+        if (image != null)
+        {
+            image.sprite = null;
+            image.color = new Color(0,0,0,0);
+            Coins.instance.ItemSold(50);
+            Coins.instance.UpdateUI();
+        }
+    }
+}
