6eefe9f7adcf47f2775a92ab78eb37233b0abf7c
ginsta6 <gintazzz9@gmail.com>
2020-06-26 01:42:18 +0300

    added money system and shop

--- Assets/Scripts/InvenotorySlot.cs
+++ Assets/Scripts/InvenotorySlot.cs
@@ -6,16 +6,59 @@ using UnityEngine.UI;
 public class InvenotorySlot : MonoBehaviour
 {
     public Image image;
+    public Item item;
 
+
+    private void Update()
+    {
+        if ((item != null) && (image.sprite == null))
+        {
+            UpdateSlot();
+        }
+    }
 
     public void SellItem()
     {
-        if (image != null)
+        if (item != null)
         {
-            image.sprite = null;
-            image.color = new Color(0,0,0,0);
-            Coins.instance.ItemSold(50);
-            Coins.instance.UpdateUI();
+            Inventory.instance.RemoveItem(item);
+            Coins.instance.ItemSold(item.price * 1.3f);
+            DeleteSlotItem();
+
         }
     }
+
+    public void BuyItem()
+    {
+        if ((item != null) && (Inventory.instance.GetCount() < 7))
+        {
+            if (Coins.instance.GetBalance() >= item.price)
+            {
+                Inventory.instance.AddItem(item);
+                Coins.instance.ItemBought(item.price);
+                DeleteSlotItem();
+            }
+        }
+    }
+
+    public void DeleteSlotItem()
+    {
+        item = null;
+        image.sprite = null;
+        image.color = new Color(0, 0, 0, 0);
+    }
+
+    public void AddItem(Item _item)
+    {
+        item = _item;
+    }
+
+    public void UpdateSlot()
+    {
+        image.sprite = item.icon;
+        image.color = new Color(255, 255, 255, 255);
+        //text
+    }
+
+
 }
